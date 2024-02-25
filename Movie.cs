7bb2ae3047babb5df5dda3d8cae23d1ab2b229a4
Zenon Marek Raubuć <m.raubuc@wp.pl>
2024-02-25 07:54:49 +0100

    Create Movie.cs

---
+++ Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro_git_for_programmers
+{
+	public class Movie
+	{
+		public string Title { get; set; }
+		public List<string> Actors { get; set; }
+	}
+}
