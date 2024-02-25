8b50885d1d28bc2a582b40883fad4fa55c8022b6
Zenon Marek Raubuć <m.raubuc@wp.pl>
2024-02-25 05:50:55 +0100

    Dodano jedną linię kodu

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Dodałem tą linijkę kodu w Visual Studio");
+Console.WriteLine("Dodałem tę linię w repozytorium wiersza poleceń");
